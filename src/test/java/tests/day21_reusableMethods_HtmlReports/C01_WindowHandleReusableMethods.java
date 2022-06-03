2702fb8490d2d18400e54de2be0b4db56bcc4d74
M.Arif Özfidan <ozfidanarif@gmail.com>
2022-06-03 23:46:53 +0200

    03.06.2022

--- src/test/java/tests/day21_reusableMethods_HtmlReports/C01_WindowHandleReusableMethods.java
+++ src/test/java/tests/day21_reusableMethods_HtmlReports/C01_WindowHandleReusableMethods.java
@@ -14,37 +14,42 @@ public class C01_WindowHandleReusableMethods {
     public void test01() {
         //● https://the-internet.herokuapp.com/windows adresine gidin.
         Driver.getDriver().get("https://the-internet.herokuapp.com/windows");
-        String ilkSayfaWH=Driver.getDriver().getWindowHandle();
+        String ilkSayfaWH = Driver.getDriver().getWindowHandle();
         //● Click Here butonuna basın.
         Driver.getDriver().findElement(By.linkText("Click Here")).click();
-        Set<String> windowHanleSeti= Driver.getDriver().getWindowHandles();
-        String ikinciSayfaWH="";
-        for (String each: windowHanleSeti
+        Set<String> windowHanleSeti = Driver.getDriver().getWindowHandles();
+        String ikinciSayfaWH = "";
+        for (String each : windowHanleSeti
         ) {
-            if (!each.equals(ilkSayfaWH)){
-                ikinciSayfaWH=each;
+            if (!each.equals(ilkSayfaWH)) {
+                ikinciSayfaWH = each;
             }
         }
         Driver.getDriver().switchTo().window(ikinciSayfaWH);
         System.out.println(Driver.getDriver().getTitle());
         // acilan yeni tab'in title'inin "New Window" oldugunu test edin
-        String expectedTitle="New Window";
-        String actualTitle=Driver.getDriver().getTitle();
-        Assert.assertEquals(actualTitle,expectedTitle);
+        String expectedTitle = "New Window";
+        String actualTitle = Driver.getDriver().getTitle();
+        Assert.assertEquals(actualTitle, expectedTitle);
     }
+
     @Test
     public void test02() {
         //● https://the-internet.herokuapp.com/windows adresine gidin.
         Driver.getDriver().get("https://the-internet.herokuapp.com/windows");
         //● Click Here butonuna basın.
+
+
         Driver.getDriver().findElement(By.linkText("Click Here")).click();
         // acilan yeni tab'in title'inin "New Window" oldugunu test edin
         ReusableMethods.switchToWindow("New Window2");
-        String expectedTitle="New Window";
-        String actualTitle=Driver.getDriver().getTitle();
 
 
-        Assert.assertEquals(actualTitle,expectedTitle);
+        String expectedTitle = "New Window";
+        String actualTitle = Driver.getDriver().getTitle();
+
+
+        Assert.assertEquals(actualTitle, expectedTitle);
         Driver.closeDriver();
     }
 }
